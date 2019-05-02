5e580b59a869c24314659dd435049411610724d7
Max <maksim.l1094@gmail.com>
2019-05-02 17:56:36 +0100

    Detecting mouse clicks, in/out of field.

--- main.py
+++ main.py
@@ -12,38 +12,40 @@ m_display = pygame.display.set_mode((m_dwidth, m_dheight))
 c_white = pygame.Color(255, 255, 255)
 c_black = pygame.Color(0, 0, 0)
 
+# Objects (Move to a better place later)
+# Field = 40 * 40 tiles
+# Tile Codes
+# 0 = regular unused tile
+field = [0] * 40
+field = [field] * 40
+
+tile_width = 10
+tile_height = 10
+
+field_width = tile_width * 40
+field_height = tile_height * 40
+
+field_start_x = (m_dwidth - field_width) / 2
+field_start_y = 125
+
 # Functions
 def draw_field():
-    # Field = 40 * 40 tiles
-    # Tile Codes
-    # 0 = regular unused tile
-    field = [0] * 40
-    field = [field] * 40
+    tile_padding_x = 1
+    tile_padding_y = 1
 
     tile_offset_x = 0
     tile_offset_y = 0
-    tile_width = 10
-    tile_height = 10
-
-    space_x = 1
-    space_y = 1
-
-    field_width = tile_width * 40 + space_x * 39
-    field_height = tile_height * 40 + space_y * 39
-
-    field_start_x = (m_dwidth - field_width) / 2
-    field_start_y = 125
 
     for yline in field:
         for x in yline:
-            tile = [field_start_x + tile_offset_x, field_start_y + tile_offset_y, tile_width, tile_height]
+            tile = [field_start_x + tile_offset_x, field_start_y + tile_offset_y, tile_width-tile_padding_x, tile_height-tile_padding_y]
             
             pygame.draw.rect(m_display, c_black, tile)
 
-            tile_offset_x += tile_width + space_x
+            tile_offset_x += tile_width
 
         tile_offset_x = 0
-        tile_offset_y += tile_height + space_y
+        tile_offset_y += tile_height
 
 # Main loop
 m_fps = 30
@@ -54,6 +56,17 @@ while not m_quit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             m_quit = True
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+
+            # Check if within field bounds            
+            if (mouse_x >= field_start_x and mouse_x <= field_start_x + field_width
+                and mouse_y >= field_start_y and mouse_y <= field_start_y + field_height):
+                
+                print('Clicked on the field!')
+            else:
+                print('Clicked outside the field!')
 
     # Clear display
     m_display.fill(c_white)
