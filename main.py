49fc7c4aa9d5b1cc0cbc37950496580a12e66aa5
Max <maksim.l1094@gmail.com>
2019-05-02 12:14:02 +0100

    Drawing a field and centering on the X

--- main.py
+++ main.py
@@ -4,13 +4,46 @@ import pygame
 pygame.init()
 pygame.display.set_caption('Application Title')
 
-m_dwidth = 800
+m_dwidth = 600
 m_dheight = 600
 m_display = pygame.display.set_mode((m_dwidth, m_dheight))
 
 # Colours
 c_white = pygame.Color(255, 255, 255)
 c_black = pygame.Color(0, 0, 0)
+
+# Functions
+def draw_field():
+    # Field = 40 * 40 tiles
+    # Tile Codes
+    # 0 = regular unused tile
+    field = [0] * 40
+    field = [field] * 40
+
+    tile_offset_x = 0
+    tile_offset_y = 0
+    tile_width = 10
+    tile_height = 10
+
+    space_x = 1
+    space_y = 1
+
+    field_width = tile_width * 40 + space_x * 39
+    field_height = tile_height * 40 + space_y * 39
+
+    field_start_x = (m_dwidth - field_width) / 2
+    field_start_y = 125
+
+    for yline in field:
+        for x in yline:
+            tile = [field_start_x + tile_offset_x, field_start_y + tile_offset_y, tile_width, tile_height]
+            
+            pygame.draw.rect(m_display, c_black, tile)
+
+            tile_offset_x += tile_width + space_x
+
+        tile_offset_x = 0
+        tile_offset_y += tile_height + space_y
 
 # Main loop
 m_fps = 30
@@ -26,7 +59,7 @@ while not m_quit:
     m_display.fill(c_white)
 
     # Draw on display
-    pygame.draw.rect(m_display, c_black, [m_dwidth/2, m_dheight/2, 30, 30])
+    draw_field()
 
     # Update display
     pygame.display.update()
